27a61d33e81fb357d811d8640e95395b8d86a3fc
phi_spiritlabs <elpulga.dev2021@gmail.com>
2022-09-25 12:34:04 +0700

    toggle nav in mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { GoThreeBars } from "react-icons/go";
 
@@ -6,29 +7,38 @@ import "./navbar.css";
 
 import { links } from "../data";
 
-const Navbar = () => (
-  <nav>
-    <div className="container nav__container">
-      <Link to="/" className="logo">
-        <img src={Logo} alt="Nav logo" />
-      </Link>
-      <ul className="nav__links">
-        {links.map(({ name, path }, index) => (
-          <li key={index}>
-            <NavLink
-              to={path}
-              className={({ isActive }) => (isActive ? "active-nav" : "")}
-            >
-              {name}
-            </NavLink>
-          </li>
-        ))}
-      </ul>
-      <button className="nav__toggle-btn">
-        <GoThreeBars />
-      </button>
-    </div>
-  </nav>
-);
+const Navbar = () => {
+  const [isNavShowing, setIsNavShowing] = useState(false);
+
+  return (
+    <nav>
+      <div className="container nav__container">
+        <Link to="/" className="logo">
+          <img src={Logo} alt="Nav logo" />
+        </Link>
+        <ul
+          className={`nav__links ${isNavShowing ? "show__nav" : "hide__nav"}`}
+        >
+          {links.map(({ name, path }, index) => (
+            <li key={index}>
+              <NavLink
+                to={path}
+                className={({ isActive }) => (isActive ? "active-nav" : "")}
+              >
+                {name}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+        <button
+          onClick={() => setIsNavShowing(!isNavShowing)}
+          className="nav__toggle-btn"
+        >
+          <GoThreeBars />
+        </button>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
